7c5ee0a2952607f37e6098c20f430b2846bbbf0b
Nicolas Lema <32146297+nicolaslema2710@users.noreply.github.com>
2019-09-16 17:22:27 -0300

    Add files via upload

---
+++ spring-boot-data-jpa/src/test/java/com/bolsadeideas/springboot/app/controllers/ClienteControllerTest.java
@@ -0,0 +1,29 @@
+package com.bolsadeideas.springboot.app.controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ClienteControllerTest {
+
+    @Test
+    public void listar() {
+
+    }
+
+    @Test
+    public void crear() {
+    }
+
+    @Test
+    public void editar() {
+    }
+
+    @Test
+    public void guardar() {
+    }
+
+    @Test
+    public void eliminar() {
+    }
+}
